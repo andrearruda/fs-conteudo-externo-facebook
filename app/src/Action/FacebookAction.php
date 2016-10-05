558394ee9ccb9aa80fa3700f3689f40dd9d3b75f
André Arruda <andrearruda82@gmail.com>
2016-10-05 16:43:43 -0300

    cache da imagem de cover adicionado novas informações da fanpage no XML

--- app/src/Action/FacebookAction.php
+++ app/src/Action/FacebookAction.php
@@ -54,7 +54,7 @@ final class FacebookAction
 
             $batch = [
                 $fb->request('GET', '/' . $this->getUserId() . '?fields=id, name, engagement, company_overview, mission, picture.type(large){url}, cover.type(large){id, source}'),
-                $fb->request('GET', '/' . $this->getUserId() . '/posts?limit=' . $this->getLength() . '&fields=id, object_id, type, created_time, updated_time, name, message, shares, source, attachments{media{image{src}}, target}, likes.limit(5).summary(true){name, picture.type(large){url}}, reactions.limit(5).summary(total_count){name, picture.type(large){url}}, comments.limit(5).summary(true){from{name, picture.type(large){url}}, message}'),
+                $fb->request('GET', '/' . $this->getUserId() . '/posts?limit=' . $this->getLength() . '&fields=id, object_id, type, created_time, updated_time, name, message, shares, source, attachments{media{image{src}}, target}, likes.limit(5).summary(true){name, picture.type(small){url}}, reactions.limit(5).summary(total_count){name, picture.type(small){url}}, comments.limit(5).summary(true){from{name, picture.type(small){url}}, message}'),
             ];
 
             /** @var $fb_batch_response /Facebook\FacebookBatchResponse */
@@ -63,13 +63,29 @@ final class FacebookAction
             $fb_data_fanpage = json_decode($fb_batch_response->getGraphNode()->getField(0)->getField('body'));
             $fb_data_posts = json_decode($fb_batch_response->getGraphNode()->getField(1)->getField('body'));
 
-            //IMAGE PAGE
-            $img_page_cover_name = $fb_data_fanpage->cover->id . '.jpg';
-            $img_page_cover_path = __DIR__ . '/../../../data/uploads/' . $img_page_cover_name;
+            //FOLDER user-id
+            $path_uploads = __DIR__ . '/../../../data/uploads/' . $fb_data_fanpage->id . '/';
+            if(!file_exists($path_uploads))
+            {
+                mkdir($path_uploads);
+            }
+
+            //IMAGE PAGE COVER
+            $img_page_cover_name = 'profile_cover_' . sha1($fb_data_fanpage->cover->source) . '.jpg';
+            $img_page_cover_path = $path_uploads . $img_page_cover_name;
             if(!file_exists($img_page_cover_path))
             {
                 $content = file_get_contents(str_replace('https://', 'http://', $fb_data_fanpage->cover->source));
                 file_put_contents($img_page_cover_path, $content);
+            }
+
+            //IMAGE PAGE PICTURE
+            $img_page_picture_name = 'profile_picture_' . sha1($fb_data_fanpage->picture->data->url) . '.jpg';
+            $img_page_picture_path = $path_uploads . $img_page_picture_name;
+            if(!file_exists($img_page_picture_path))
+            {
+                $content = file_get_contents(str_replace('https://', 'http://', $fb_data_fanpage->picture->data->url));
+                file_put_contents($img_page_picture_path, $content);
             }
 
             $data = array(
@@ -88,8 +104,8 @@ final class FacebookAction
                         'full' => $fb_data_fanpage->mission
                     ),
                     'midia' => array(
-                        'cover' => $this->getPaths()['upload_path_virtual'] . $img_page_cover_name,
-                        'profile' => $this->getPaths()['upload_path_virtual'] . (new CheckHashCachedFile($fb_data_fanpage->picture->data->url))->checkHashFile(),
+                        'cover' => $this->getPaths()['upload_path_virtual'] . $fb_data_fanpage->id . '/' . $img_page_cover_name,
+                        'profile' => $this->getPaths()['upload_path_virtual'] . $fb_data_fanpage->id . '/'  . $img_page_picture_name,
                     )
                 ),
                 'feeds' => array()
@@ -105,36 +121,25 @@ final class FacebookAction
                 $updated->setTimezone(new \DateTimeZone('America/Sao_paulo'));
 
                 //IMAGE FEED
-                $cache_feed_name = $item->id . '.cache';
-                $cache_feed_path = __DIR__ . '/../../../data/uploads/' . $cache_feed_name;
-                if(!file_exists($cache_feed_path))
+                $img_feed_name = 'feed_image_' . sha1($item->attachments->data[0]->media->image->src) . '.jpg';
+                $img_feed_path = $path_uploads . $img_feed_name;
+                if(!file_exists($img_feed_path))
                 {
                     $content = file_get_contents(str_replace('https://', 'http://', $item->attachments->data[0]->media->image->src));
-
-                    $file_info = new \finfo(FILEINFO_MIME_TYPE);
-                    $mime_type = $file_info->buffer($content);
-
-                    $mimes = new \Mimey\MimeTypes;
-                    $extension = $mimes->getExtension($mime_type);
-
-                    file_put_contents(
-                        $cache_feed_path,
-                        json_encode(
-                            array(
-                                'file' => $item->id . '.' . $extension
-                            )
-                        )
-                    );
-
-                    $img_feed_name = $item->id . '.' . $extension;
-                    $img_feed_path = __DIR__ . '/../../../data/uploads/' . $img_feed_name;
-                    file_put_contents(
-                        $img_feed_path,
-                        $content
-                    );
+                    file_put_contents($img_feed_path, $content);
                 }
 
-                $cache_file = json_decode(file_get_contents($cache_feed_path));
+                //VIDEO FEED
+                if($item->type == 'video')
+                {
+                    $video_feed_name = 'feed_video_' . sha1($item->attachments->data[0]->media->image->src) . '.mp4';
+                    $video_feed_path = $path_uploads . $video_feed_name;
+                    if(!file_exists($video_feed_path))
+                    {
+                        $content = file_get_contents(str_replace('https://', 'http://', $item->source));
+                        file_put_contents($video_feed_path, $content);
+                    }
+                }
 
                 $data['feeds'][$i] = array(
                     'id' => $item->id,
@@ -148,8 +153,8 @@ final class FacebookAction
                         'full' => $item->message
                     ),
                     'midia' => array(
-                       'image' => $this->getPaths()['upload_path_virtual'] . $cache_file->file,
-                        'video' => $item->type == 'video' ? $item->source : ''
+                       'image' => $this->getPaths()['upload_path_virtual'] . $fb_data_fanpage->id . '/' . $img_feed_name,
+                        'video' => $item->type == 'video' ? $this->getPaths()['upload_path_virtual'] . $fb_data_fanpage->id . '/' . $video_feed_name : ''
                     ),
                     'engagement' => array(
                         'shares' => array(
@@ -172,25 +177,52 @@ final class FacebookAction
 
                 foreach($item->likes->data as $user)
                 {
+                    //IMAGE FEED ENGAGEMENT
+                    $img_feed_engagement_name = 'feed_engagement_' . sha1($user->picture->data->url) . '.jpg';
+                    $img_feed_engagement_path = $path_uploads . $img_feed_engagement_name;
+                    if(!file_exists($img_feed_engagement_path))
+                    {
+                        $content = file_get_contents(str_replace('https://', 'http://', $user->picture->data->url));
+                        file_put_contents($img_feed_engagement_path, $content);
+                    }
+
                     $data['feeds'][$i]['engagement']['likes']['users'][] = array(
                         'name' => $user->name,
-                        'picture' => $user->picture->data->url
+                        'picture' => $this->getPaths()['upload_path_virtual'] . $fb_data_fanpage->id . '/' . $img_feed_engagement_name
                     );
                 }
 
                 foreach($item->reactions->data as $user)
                 {
+                    //IMAGE FEED ENGAGEMENT
+                    $img_feed_engagement_name = 'feed_engagement_' . sha1($user->picture->data->url) . '.jpg';
+                    $img_feed_engagement_path = $path_uploads . $img_feed_engagement_name;
+                    if(!file_exists($img_feed_engagement_path))
+                    {
+                        $content = file_get_contents(str_replace('https://', 'http://', $user->picture->data->url));
+                        file_put_contents($img_feed_engagement_path, $content);
+                    }
+
                     $data['feeds'][$i]['engagement']['reactions']['users'][] = array(
                         'name' => $user->name,
-                        'picture' => $user->picture->data->url
+                        'picture' => $this->getPaths()['upload_path_virtual'] . $fb_data_fanpage->id . '/' . $img_feed_engagement_name
                     );
                 }
 
                 foreach($item->comments->data as $user)
                 {
+                    //IMAGE FEED ENGAGEMENT
+                    $img_feed_engagement_name = 'feed_engagement_' . sha1($user->from->picture->data->url) . '.jpg';
+                    $img_feed_engagement_path = $path_uploads . $img_feed_engagement_name;
+                    if(!file_exists($img_feed_engagement_path))
+                    {
+                        $content = file_get_contents(str_replace('https://', 'http://', $user->from->picture->data->url));
+                        file_put_contents($img_feed_engagement_path, $content);
+                    }
+
                     $data['feeds'][$i]['engagement']['comments']['users'][] = array(
                         'name' => $user->from->name,
-                        'picture' => $user->from->picture->data->url,
+                        'picture' => $this->getPaths()['upload_path_virtual'] . $fb_data_fanpage->id . '/' . $img_feed_engagement_name,
                         'message' => array(
                             'cut' => str_replace("\n", ' ', (string) Stringy::create($user->message)->safeTruncate(250, '...')),
                             'full' => $user->message
